6790eb7bc2e432fc1fad87def1972b7d9a5c3773
Chandrika1303 <chandrikapothala7@gmail.com>
2025-03-10 13:12:20 +0530

    Add files via upload

---
+++ Java basics/main5.java
@@ -0,0 +1,10 @@
+public class main5 { 
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+        System.out.println("Java Comments Example");
+        printGreeting();
+    }
+    public static void printGreeting() {
+        System.out.println("Welcome to Java Programming!");
+    }
+}
